8acd4bb437610689c9f37a317d18ebf77e164d2d
cpt409 <chrisschoolacct@yahoo.com>
2020-10-15 13:18:25 -0400

    Add project files.

---
+++ zTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
